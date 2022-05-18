db245f5cba53b78c843ec5ca85aa024261601e3d
Erik Busch <erik_busch@gmx.de>
2022-05-18 16:14:52 +0200

    switch case aufgabe

--- src/main/java/ControlFlow.java
+++ src/main/java/ControlFlow.java
@@ -4,9 +4,31 @@ public class ControlFlow {
         if (customers > 30) {
             System.out.println("Zu viele Personen");
             return true;
-        }else{
+        } else {
             System.out.println("Maximale Personenzahl nicht ueberschritten!");
             return false;
         }
+    }
+
+
+    public static String checkTooManyCustomers(String alert, int customer) {
+        switch (alert) {
+            case "rot":
+                return "Keine Personen erlaubt";
+            case "gelb":
+                if(customer > 30) {
+                    return "Zu viele Personen";
+                }
+                return "Max Anzahl nicht erreicht";
+            case "gruen":
+                if(customer > 60) {
+                    return "Zu viele Personen";
+                }
+                return "Max Anzahl nicht erreicht";
+            default:
+                return "unknown alarm level";
+        }
+
+
     }
 }
--- src/test/java/ControlFlowTest.java
+++ src/test/java/ControlFlowTest.java
@@ -49,4 +49,63 @@ class ControlFlowTest {
         Assertions.assertFalse(actual);
     }
 
+    @Test
+    void checkForAlarmcolor() {
+        // Given
+
+        String alert = "rot";
+        int customers = 10;
+        //When
+
+        String actual = ControlFlow.checkTooManyCustomers(alert,customers);
+
+        //Then
+
+        Assertions.assertEquals("Keine Personen erlaubt",actual);
+    }
+    @Test
+    void checkForAlarmcolorAndMoreThan30Customers() {
+        // Given
+
+        String alert = "gelb";
+        int customers = 32;
+        //When
+
+        String actual = ControlFlow.checkTooManyCustomers(alert,customers);
+
+        //Then
+
+        Assertions.assertEquals("Zu viele Personen",actual);
+    }
+
+    @Test
+    void checkForAlarmcolorAndLessThan30Customers() {
+        // Given
+
+        String alert = "gelb";
+        int customers = 23;
+        //When
+
+        String actual = ControlFlow.checkTooManyCustomers(alert,customers);
+
+        //Then
+
+        Assertions.assertEquals("Max Anzahl nicht erreicht",actual);
+    }
+
+    @Test
+    void checkForAlarmcolorAndLessThan60Customers() {
+        // Given
+
+        String alert = "gruen";
+        int customers = 59;
+        //When
+
+        String actual = ControlFlow.checkTooManyCustomers(alert,customers);
+
+        //Then
+
+        Assertions.assertEquals("Max Anzahl nicht erreicht",actual);
+    }
+
 }
